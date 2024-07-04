9bc853112f58e9ee9d3f84be882485473f15e9fb
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-04 10:54:14 +0300

    Update encrypt_password.py

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""encrypt_password module"""
+
+import bcrypt
+
+
+def hash_password(password):
+    # Generate a salt and hash the password
+    salt = bcrypt.gensalt()
+
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
