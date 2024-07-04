23812210b191559e021db348242a09ea280ed21f
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-04 10:57:14 +0300

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,9 +5,13 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """Salted pass generation"""
+    """Salted password generation"""
     # Generate a salt and hash the password
     salt = bcrypt.gensalt()
-
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if a password matches its hashed version"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
