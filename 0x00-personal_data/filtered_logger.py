4bc1498c085fddcaf191c530c6502ffaf1ebdbac
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-04 09:52:58 +0300

    task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""filtered_logger module"""
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """Obfuscate specified fields in a log message."""
+    for field in fields:
+        message = re.sub(rf"{field}=(.*?)\{separator}",
+                         f'{field}={redaction}{separator}', message)
+    return message
