544cf8444cfee8b2723d759cac62adf7006b2bcc
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-04 10:46:38 +0300

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -61,11 +61,33 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     database = os.getenv("PERSONAL_DATA_DB_NAME")
 
-    conn = mysql.connector.connect(
+    return mysql.connector.connect(
         user=username,
         password=password,
         host=host,
         database=database
     )
 
-    return conn
+
+def main():
+    """Main function to read and filter data."""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+
+    logger = get_logger()
+
+    for row in cursor.fetchall():
+        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
+            f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
+            f"last_login={row[6]}; user_agent={row[7]};"
+        log_record = logging.LogRecord(
+            "user_data", logging.INFO, None, None, message, None, None)
+        print(logger.format(log_record))
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
