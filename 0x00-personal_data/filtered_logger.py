d2e15afe2c94b3b6882b9fe3fbede98dad6b3630
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-04 10:28:51 +0300

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """filtered_logger module"""
 
-import re
-from typing import List
 import logging
+import re
+import os
+import mysql.connector
+from typing import List
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -37,6 +39,7 @@ class RedactingFormatter(logging.Formatter):
                             original_message,
                             self.SEPARATOR)
 
+
 def get_logger() -> logging.Logger:
     """Create and configure a logger."""
     logger = logging.getLogger("user_data")
@@ -49,3 +52,20 @@ def get_logger() -> logging.Logger:
     logger.addHandler(handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Connect to a secure database."""
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    database = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    conn = mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database
+    )
+
+    return conn
