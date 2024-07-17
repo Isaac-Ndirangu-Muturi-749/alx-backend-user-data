6198f9097aa659874a76aebb1e38daabe064645b
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-17 08:38:11 +0300

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -52,3 +52,13 @@ class DB:
             raise NoResultFound(
                 "No result found for the given filter criteria.")
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user's attributes
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError(f"User has no attribute '{key}'")
+            setattr(user, key, value)
+        self._session.commit()
