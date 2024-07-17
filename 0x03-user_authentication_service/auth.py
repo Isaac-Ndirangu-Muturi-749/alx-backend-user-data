703f4b42f49f02ac5b86446c3e2d7633b90c486d
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-17 10:47:02 +0300

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -46,3 +46,13 @@ class Auth:
     def _generate_uuid(self) -> str:
         """Generate a new UUID and return it as a string"""
         return str(uuid.uuid4())
+
+    def create_session(self, email: str) -> str:
+        """Create a session ID for a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = self._generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
