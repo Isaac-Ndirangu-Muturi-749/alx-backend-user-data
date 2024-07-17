71ea5d3249b418220ea264e4630887dc44783d0a
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-17 19:25:46 +0300

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,23 +5,28 @@ auth module
 
 from db import DB
 from user import User
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 import uuid
+
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password using bcrypt."""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+def _generate_uuid() -> str:
+    """Generate a new UUID and return it as a string"""
+    return str(uuid.uuid4())
 
 
 class Auth:
     """Auth class to interact with the authentication database."""
 
     def __init__(self):
+        """init"""
         self._db = DB()
-
-    def _hash_password(self, password: str) -> bytes:
-        """Hash a password using bcrypt."""
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-        return hashed_password
 
     def register_user(self, email: str, password: str) -> User:
         """Register a new user with the provided email and password."""
@@ -29,7 +34,7 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
             return new_user
 
@@ -43,15 +48,11 @@ class Auth:
         except NoResultFound:
             return False
 
-    def _generate_uuid(self) -> str:
-        """Generate a new UUID and return it as a string"""
-        return str(uuid.uuid4())
-
     def create_session(self, email: str) -> str:
         """Create a session ID for a user"""
         try:
             user = self._db.find_user_by(email=email)
-            session_id = self._generate_uuid()
+            session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
             return session_id
         except NoResultFound:
@@ -79,7 +80,7 @@ class Auth:
         """get reset password_token"""
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = self._generate_uuid()
+            reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
         except NoResultFound:
@@ -89,7 +90,7 @@ class Auth:
         """update password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             self._db.update_user(user.id,
                                  hashed_password=hashed_password,
                                  reset_token=None)
