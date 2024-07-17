345d72e7d1404c0d563e139f545850fa9123813e
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-17 09:41:43 +0300

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -31,3 +31,13 @@ class Auth:
             hashed_password = self._hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validates user login"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(
+                password.encode('utf-8'),
+                user.hashed_password)
+        except NoResultFound:
+            return False
