86d3b5c4f21404f6ed283ce9d4ac2c4c20fb6762
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-17 19:39:26 +0300

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -82,21 +82,20 @@ def get_reset_password_token():
 
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
-def update_password() -> str:
-    """PUT /reset_password
-    Updates the password using the reset token.
-    """
-    try:
-        email = request.form['email']
-        reset_token = request.form['reset_token']
-        new_password = request.form['new_password']
-    except KeyError:
+def update_password():
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+    if not email or not reset_token or not new_password:
         abort(400)
 
     try:
         AUTH.update_password(reset_token, new_password)
     except ValueError:
         abort(403)
+
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
