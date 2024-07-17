f22b6cc9c71542819de2edc4dbf14aabfed83c61
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-17 19:09:35 +0300

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,44 +3,49 @@
 DB module
 """
 
-#!/usr/bin/env python3
-"""db.py - Database module"""
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
 from user import Base, User
 
 
 class DB:
-    """DB Class for Object-Relational Mapping."""
+    """DB class
+    """
 
-    def __init__(self):
-        """Initialize the database connection and create tables."""
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self):
-        """Session Getter Method."""
+    def _session(self) -> Session:
+        """Memoized session object
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a user to the database."""
+        """Add a new user to the database
+        """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
 
+
     def find_user_by(self, **kwargs) -> User:
-        """Find a user by specified key-value pairs."""
+        """Find a user by arbitrary keyword arguments
+        """
         if not kwargs:
             raise InvalidRequestError
 
@@ -57,15 +62,11 @@ class DB:
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """Update user attributes."""
+        """Update a user's attributes
+        """
         user = self.find_user_by(id=user_id)
-
-        column_names = User.__table__.columns.keys()
-        for key in kwargs.keys():
-            if key not in column_names:
-                raise ValueError
-
         for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError(f"User has no attribute '{key}'")
             setattr(user, key, value)
-
         self._session.commit()
